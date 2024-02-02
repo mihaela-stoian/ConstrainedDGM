183d469e1c6d75562f286904f2c0b1bf0f0f4aa2
Mihaela Catalina <c.mihaela.stoian@gmail.com>
2024-02-02 11:48:04 +0000

    refactored

--- other_helper_scripts/test_heloc_cons.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-from constraints.heloc import evaluate_numpy_heloc
-from evaluation.constraints import constraint_satisfaction
-
-df = pd.read_csv("data/heloc/train_data.csv")
-constraint_satisfaction(df.to_numpy(), use_case="heloc")
